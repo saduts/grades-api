4a95941417e8026364cbe36fd3a2bc896d4cd93d
sadu.souza <saduts@gmail.com>
2020-09-06 20:43:33 -0300

    Procfile

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'http://mighty-journey-42961.herokuapp.com',
   })
 );
 
@@ -30,4 +30,4 @@ app.get('/', (req, res) => {
   res.send('API em execucao');
 });
 
-app.listen(process.env.PORT || 8081, () => {});
+app.listen(process.env.PORT || 8081, () => { });
