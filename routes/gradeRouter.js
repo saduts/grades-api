9775c923083160711ae9e2d5453d63cba6d1e16e
sadu.souza <saduts@gmail.com>
2020-09-06 21:06:54 -0300

    Correçao das rotas

--- routes/gradeRouter.js
+++ routes/gradeRouter.js
@@ -3,11 +3,11 @@ import controller from '../controllers/gradeController.js';
 
 const app = express();
 
-app.post('/grade/', controller.create);
-app.get('/grade/', controller.findAll);
+app.post('/grade', controller.create);
+app.get('/grade', controller.findAll);
 app.get('/grade/:id', controller.findOne);
 app.put('/grade/:id', controller.update);
 app.delete('/grade/:id', controller.remove);
-app.delete('/grade/', controller.removeAll);
+app.delete('/grade', controller.removeAll);
 
 export { app as gradeRouter };
